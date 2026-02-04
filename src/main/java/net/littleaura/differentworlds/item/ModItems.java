565faa2a131239671916decdd5f9eb273f56eaa9
littleaura <juri.guguran@gmail.com>
2026-02-04 21:12:15 +0100

    Initial commit

---
+++ src/main/java/net/littleaura/differentworlds/item/ModItems.java
@@ -0,0 +1,35 @@
+package net.littleaura.differentworlds.item;
+
+import net.littleaura.differentworlds.DifferentWorlds;
+import net.minecraft.core.Registry;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.Identifier;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.item.Item;
+
+import java.util.function.Function;
+
+public class ModItems {
+
+    public static final Item MOONLIT_JADE_SHARD = register("moonlit_jade_shard", Item::new, new Item.Properties());
+
+
+
+
+    public static <GenericItem extends Item> GenericItem register(String name, Function<Item.Properties, GenericItem> itemFactory, Item.Properties settings) {
+        // Create the item key.
+        ResourceKey<Item> itemKey = ResourceKey.create(Registries.ITEM, Identifier.fromNamespaceAndPath(DifferentWorlds.MOD_ID, name));
+
+        // Create the item instance.
+        GenericItem item = itemFactory.apply(settings.setId(itemKey));
+
+        // Register the item.
+        Registry.register(BuiltInRegistries.ITEM, itemKey, item);
+
+        return item;
+    }
+
+    public static void initialize() {
+    }
+}
